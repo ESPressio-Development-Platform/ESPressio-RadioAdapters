40fbe03a2b59089e3d017613770c008844a8da08
Simon J Stuart <lakraven@gmail.com>
2026-09-13 13:54:08 +0200

    Expose direct Radio lower transport

--- src/ESPressio_RadioAdapters.hpp
+++ src/ESPressio_RadioAdapters.hpp
@@ -3,4 +3,5 @@
 #include "ESPressio_RadioAdapterBinding.hpp"
 #include "ESPressio_RadioAdapterEnvelope.hpp"
 #include "ESPressio_RadioAdapterIngress.hpp"
+#include "ESPressio_RadioAdapterLowerTransport.hpp"
 #include "ESPressio_RadioAdapterServiceMapping.hpp"
